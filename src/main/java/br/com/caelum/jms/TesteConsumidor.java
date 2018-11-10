6f53b829d643f239b36a9725864b5e485f922b48
Fabio Santana <fabio.fcsantana@gmail.com>
2018-11-10 20:32:21 -0200

    Add consumer

--- src/main/java/br/com/caelum/jms/TesteConsumidor.java
+++ src/main/java/br/com/caelum/jms/TesteConsumidor.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
@@ -13,13 +17,23 @@ public class TesteConsumidor {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws NamingException, JMSException {
 		InitialContext context = new InitialContext();
-		ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("ConnectionFactory");
-		Connection connection = connectionFactory.createConnection();
 		
+		ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		
+		Connection connection = connectionFactory.createConnection();
 		connection.start();
 		
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination destination = (Destination) context.lookup("financeiro");
+		MessageConsumer consumer = session.createConsumer(destination);
+		
+		Message message = consumer.receive();
+		System.out.println("Mensagem recebida: [" + message + "]");
+
 		new Scanner(System.in).nextLine();
 		
+		session.close();
 		connection.close();
 		context.close();
 	}
